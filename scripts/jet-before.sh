010e7ee62e917c680460f29e21252152df0f20c0
PunKeel <punkeel@me.com>
2017-06-13 11:25:21 +0200

    Create jet-before.sh

---
+++ scripts/jet-before.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [ -n "${TRAVIS_TAG}" ] || [ "${TRAVIS_PULL_REQUEST}" = "true" ]
+  then
+    echo "Skipping the Excelsior Jet build."
+    return
+fi
+if [ -n "${JET_URL}" ] || [ "${TRAVIS_PULL_REQUEST}" = "true" ]
+  then
+    echo "Unknown JET_URL variable, skipping build."
+    return
+fi
+
+wget -q --no-check-certificate -O jet-1130-pro-en-linux-x86.bin "${JET_URL}"
+chmod +x ./jet-1130-pro-en-linux-x86.bin
+./jet-1130-pro-en-linux-x86.bin -batch -no-aftrun
